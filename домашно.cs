184d6cc630018225a85f0c4166d2f755c8c2d96b
Velislav9 <78847088+Velislav9@users.noreply.github.com>
2021-09-26 21:48:51 +0300

    Add files via upload

---
+++ домашно.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bin = Console.ReadLine();
+            int dec = Convert.ToInt32(bin, 2);
+            string xec = Console.ReadLine();
+            int dec1 = Convert.ToInt32(xec, 16);
+            int SUMA = dec1 + dec;
+            Console.WriteLine(SUMA);
+        }
+    }
+}
